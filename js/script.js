8bdd63c185735962dc18f3d555b1f47fde1f2fb1
Aleksandra Joachimiak <joachim07@poczta.onet.pl>
2020-07-18 23:28:06 +0200

    Modify script.js, correct errors with link-gener.

--- js/script.js
+++ js/script.js
@@ -1,53 +1,40 @@
 'use strict';
 
-const templates = {
-  articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML)
-}
-
 const titleClickHandler = function(event) {
   event.preventDefault();
-  	const clickedElement = this;
-  	console.log('Link was clicked!');
+  const clickedElement = this;
+  console.log('Link was clicked!');
   console.log(event);
 
-  	/* [1] remove class 'active' from all article links  */
-
-  	const activeLinks = document.querySelectorAll('.titles a.active');
-
-  	for(let activeLink of activeLinks) {
-    	activeLink.classList.remove('active');
-  	}
-
-  	/* [2] add class 'active' to the clicked link */
-
-  	clickedElement.classList.add('active');
-  	console.log('clickedLink: ', clickedElement);
-
-  	/* [3] remove class 'active' from all articles */
-  
-  	const activeArticles = document.querySelectorAll('.posts article.active');
-
-  for(let activeArticle of activeArticles) {
-  		activeArticle.classList.remove('active');
+  /* [1] remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for(let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
   }
 
-  	/* [4] get 'href' attribute from the clicked link */
-  
-  	const articleSelector = clickedElement.getAttribute('href');
+  /* [2] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedLink: ', clickedElement);
 
-  	/* [5] find the correct article using the selector (value of 'href' attribute) */
-  
-  	const targetArticle = document.querySelector(articleSelector);
-  	console.log(targetArticle);
+  /* [3] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active');
+  for(let activeArticle of activeArticles) {
+  	activeArticle.classList.remove('active');
+  }
 
-  	/* [6] add class 'active' to the correct article */
-  
+  /* [4] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+
+  /* [5] find the correct article using the selector (value of 'href' attribute) */ 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
+  /* [6] add class 'active' to the correct article */
   targetArticle.classList.add('active');
 };
 
 
 /* 2 część skryptu - chcemy generować nową listę linków po każdym kliknięciu tagu (ze składnikami) lub rodzaju dania */
-
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
@@ -55,32 +42,24 @@ const optArticleSelector = '.post',
 function generateTitleLinks(customSelector = '') {
 
   /* [1] remove contents of titleList */
-		
   const titleList = document.querySelector(optTitleListSelector);
-  console.log('optTitleListSelector: ', titleList);
-			
+  console.log('optTitleListSelector: ', titleList);		
   titleList.innerHTML = '';
 
-  /* [2] for each article */
-		
+  /* [2] for each article */		
   const articles = document.querySelectorAll(optArticleSelector + customSelector);
-
   for(let article of articles) {
     console.log('article: ', article);
 
     /* [-2a-] get the article id */
-		
     const articleId = article.getAttribute('id');
     console.log('articleId: ', articleId);
 
-    /* [-2b-] find the title element & get the title from the title element */
-		
+    /* [-2b-] find the title element & get the title from the title element */	
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* [-2c-] create HTML of the link & insert link into titleList */
-		
-    const linkHTMLData = {id: articleId, title: articleTitle};
-    const linkHTML = templates.articleLink(linkHTMLData);
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
     	titleList.innerHTML = titleList.innerHTML + linkHTML;
   }
 
@@ -88,13 +67,11 @@ function generateTitleLinks(customSelector = '') {
   for(let link of links) {
   		link.addEventListener('click', titleClickHandler);
   }
-
 }
 generateTitleLinks();
 
 
 // 3. część zadania, generowanie listy tagów;
-
 const optArticleTagsSelector = '.post-tags .list-horizontal' 
 
 // Akcja po kliknięciu w tag składnika(dania)
@@ -112,7 +89,6 @@ function tagClickHandler(event){
 
   /* find all tag links with class active */
   const activeLinks = document.querySelectorAll('a.active[href^="#tag-"]');
-  
   for(let activeLink of activeLinks) {
 	activeLink.classList.remove('active');
   }
@@ -128,58 +104,7 @@ function tagClickHandler(event){
 }
 
 
-// cz.4. dodanie listy: Rodzaje dań
-function generateMeal(){
-  const articles = document.querySelectorAll(optArticleSelector); 
-  
-  for(let article of articles) {
-	const mealWraper = article.querySelector('.post-meal'); // meals wrapper
-	const meal = article.getAttribute('data-meal');
-	const linkHTML = '<a href="#' + meal + '"><span>' + meal + '</span></a>';
-	mealWraper.innerHTML = mealWraper.innerHTML + linkHTML;
-  }
-}
-generateMeal();
-
-// Akcja po kliknięciu w rodzaj dania (meal)
-function mealClickHandler(event){
-  event.preventDefault();
-  const clickedElement = this;
-  console.log('Link was clicked!');
-
-  /* make a new constant "href" and read the attribute "href" of the clicked element */
-  const href = clickedElement.getAttribute('href');
-  console.log('href: ', href);
-
-  /* make a new constant "meal" and extract meal from the "href" constant */
-  const meal = href.replace('#meal-', '');
-
-  const activeLinks = document.querySelectorAll('.post-meal a.active');
-  for(let activeLink of activeLinks) {
-	activeLink.classList.remove('active');
-  }
-
-  /* find all meal links with "href" attribute equal to the "href" constant */
-  const mealLinks = document.querySelectorAll('a[href="#meal-'+ href +'"]');
-  
-  for(let mealLink of mealLinks){
-	mealLink.classList.add('active');
-  }
-
-  function addClickListenersToMeal(){  
-    const links = document.querySelectorAll('.post-meal');
-    for(let link of links) {
-  		link.addEventListener('click', tagClickHandler);
-    }
-  }
-  addClickListenersToMeal();
-
-  generateTitleLinks('[data-meal="' + meal + '"]');  
-}
-
-
-// cz. 5. Wyświetlanie chmury tagów w prawej kolumnie
-
+// cz. 4. Wyświetlanie chmury tagów w prawej kolumnie
 function calculateTagsParams(tags) {
 	const params =
 	{
@@ -269,22 +194,57 @@ function generateTagsCloud(){
 	
     allTagsHTML += tagLinkHTML;
   }
+
   /* add HTML from allTagsHTML to tagList */
   tagList.innerHTML = allTagsHTML;
 
   const links = document.querySelectorAll('a[href^="#tag-"]');
-
   for(let link of links) {
   	link.addEventListener('click', tagClickHandler);
   }
-
 }
 generateTagsCloud();
 
 
-// cz. 6. Wygenerowanie listy rodzajów dań w prawej kolumnie, pod chmurą tagów
+// cz.5. dodanie listy: Rodzaje dań po tytule artykułu (środkowa kol.)
 
-const optMealsListSelector = '.meals.list' //by znaleźć listę rodzajów dań w prawej kol.
+function mealClickHandler(event){
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
+
+  /* make a new constant "href" and read the attribute "href" of the clicked element */
+  const href = clickedElement.getAttribute('href');
+  console.log('href: ', href);
+
+  /* make a new constant "meal" and extract tag from the "href" constant */
+  const meal = href.replace('#meal-', '');
+
+  const activeLinks = document.querySelectorAll(href);
+  for(let activeLink of activeLinks) {
+	activeLink.classList.remove('active');
+  }
+
+  /* find all meal links with "href" attribute equal to the "href" constant */
+  const mealLinks = document.querySelectorAll('a[href="#meal-'+ href +'"]');
+  
+  for(let mealLink of mealLinks){  //LOOP: for each found tag link
+	mealLink.classList.add('active');
+  }
+
+  function addClickListenersToMeal(){  
+    const links = document.querySelectorAll('a[href^="#meal-"]');
+    for(let link of links) {
+  		link.addEventListener('click', mealClickHandler);
+    }
+  }
+  addClickListenersToMeal();
+
+  generateTitleLinks('[data-meal="' + meal + '"]');
+}
+
+
+// cz. 6. Wygenerowanie listy rodzajów dań w prawej kolumnie, pod chmurą tagów
 
 function generateMeals(){
 	
@@ -297,7 +257,7 @@ function generateMeals(){
 	
 	let html = '';
 	const meal = article.getAttribute('data-meal');
-	const linkHTML = '<a href="#' + meal + '"><span>' + meal + '</span></a>';
+	const linkHTML = '<a href="#meal-' + meal + '"><span>' + meal + '</span></a>';
 	html = html + linkHTML;
 		
 	/*  check if this link is NOT already in allMeals */
@@ -328,7 +288,7 @@ function generateMeals(){
     allMealsHTML += mealLinkHTML;
   }
 
-  /* add HTML from allTagsHTML to tagList */
+  /* add HTML from allTagsHTML to mealList */
   mealList.innerHTML = allMealsHTML;	
 
   const links = document.querySelectorAll('a[href^="#meal-"]');
